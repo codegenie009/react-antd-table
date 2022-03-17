e70944e9c83af092cbc8b15acb483cc6f5683952
gs <ghostson1120@outlook.com>
2022-03-17 10:22:00 +0800

    antd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import FixedTable from './components/antd-table';
+function App() {
+  return (
+    <div className="App">
+      <FixedTable />
+    </div>
+  );
+}
+
+export default App;
